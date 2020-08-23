9571ba126588fb4b52c76dd8574e304217868dda
yuval-levin <57755465+yuval-levin@users.noreply.github.com>
2020-08-23 18:01:16 +0300

    MatrixShift
    Added two new helper functions for matrixShift:
    one_norm - calculates matrix's 1-norm.
    AddScalarMatrix - adds b*Id to a given matrix, b is a scalar

---
+++ MatrixShift.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* method to calculate the 1-norm of matrix mat.
+//TODO: check if double is necessary */
+double one_norm(double *mat,int rows,int cols)
+{
+	int i;
+	int j;
+	double* matPointer;
+	int maxSum = 0;
+	int currentSum;
+	
+	for (i = 0; i < cols; i++) 
+	{
+		currentSum = 0 ;
+		for (j = 0; j < rows; j++)
+		{
+			matPointer=mat+(i*cols);
+			currentSum+=*matPointer;
+		}
+		if(currentSum > maxSum)
+		{
+			maxSum = currentSum;
+		}	
+	}
+	return maxSum;
+}
+
+/*given a matrix mat,and a scalar, add to mat Id*scalar */
+//TODO: make sure it is okay to alter the matrix.
+void AddScalarMatrix(double* mat,int rows,int cols,double scalar)
+{
+	int i;
+	int j;
+	double* matPointer;
+	for (i = 0;i < rows;i++)
+	{
+		matPointer=mat+(i*cols)+i;
+		*matPointer=*matPointer+scalar;
+	}
+	
+}
