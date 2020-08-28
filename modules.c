b39f005adace78b91d82ef3058c9e7e3e33efde2
yuval-levin <57755465+yuval-levin@users.noreply.github.com>
2020-08-28 12:59:31 +0300

    divisionGroup  struct
    Added new struct to represent divisonGroup

--- modules.c
+++ modules.c
@@ -2,16 +2,30 @@
 #include "spmat.h"
 
 
-## RECONSIDER LOCATION OF THOSE STRUCTS, NOTE THEY'RE DEFINED HERE AND IN HEADER - NOT SURE WHERE IS CORRECT
+//RECONSIDER LOCATION OF THOSE STRUCTS, NOTE THEY'RE DEFINED HERE AND IN HEADER - NOT SURE WHERE IS CORRECT
 struct graph 
 {
   struct spmat_ll A;
   long* vectorDegrees;
   long M;
 }
-
+/*Represents a group of divisions*/
 struct division
 {
-	int len  //number of divisions
-	int** divisions //"P" in algorithm
-}
+	int len;  //number of divisions
+	struct divisionGroup* divisions; //"P" in algorithm
+};
+
+/*represents a single group in divisions*
+ * TODO: change short to bit? or perhaps boolean? need to see who's smaller*/
+struct divisionGroup
+{
+	int groupSize;
+	short* groupVector;
+	/* For n number of nodes in graph , group vector is size n
+	 * groupVector[i]=0 if node i not in group. 1 if in group.
+	 * #{i | groupVector[i]=1 } = groupSize
+	*/
+};
+
+#endif
--- modules.h
+++ modules.h
@@ -8,10 +8,23 @@ struct graph
   long M;
 };
 
+/*Represents a group of divisions*/
 struct division
 {
 	int len;  //number of divisions
-	int** divisions; //"P" in algorithm
+	struct divisionGroup* divisions; //"P" in algorithm
+};
+
+/*represents a single group in divisions*
+ * TODO: change short to bit? or perhaps boolean? need to see who's smaller*/
+struct divisionGroup
+{
+	int groupSize;
+	short* groupVector;
+	/* For n number of nodes in graph , group vector is size n
+	 * groupVector[i]=0 if node i not in group. 1 if in group.
+	 * #{i | groupVector[i]=1 } = groupSize
+	*/
 };
 
 #endif
