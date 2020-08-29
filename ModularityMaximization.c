81552ec25a7ffbf41914793789ea9d3b285a8817
yuval-levin <57755465+yuval-levin@users.noreply.github.com>
2020-08-29 16:52:55 +0300

    ModMax Helper functions
    created unmovedList

--- ModularityMaximization.c
+++ ModularityMaximization.c
@@ -6,9 +6,16 @@
 void modularityMaximization(int* vectorS, struct divisionGroup* g)
 {
 	const double epsilon = 0.00001;
-	double modularityChange;
+	double modularityChange,Q0;
+	int i;
+	struct node* unmoved = createUnmovedList(g->groupSize);
+
 	do
 	{
+		for(i = 0;i< g->groupSize;i++)
+		{
+
+		}
 
 	}while(modularityChange > epsilon);
 }
@@ -16,10 +23,23 @@ void modularityMaximization(int* vectorS, struct divisionGroup* g)
 struct node* createUnmovedList(int sizeOfg)
 {
 	int i;
-	struct node* head,prev;
-	head =(struct node*)malloc(sizeof(struct node*))
-	head->data.num = 0;
-	head->next = NULL;
-	prev = head;
-	for(i = 1;i < sizeOfg;i++)
+	struct node* head,prev = NULL;
+	for(i = 0;i < sizeOfg;i++)
+		{
+			prev = appendToList(prev,i);
+			if (i == 0) head = prev;
+		}
+	return head;
+}
+
+struct node* appendToList(struct node* prev, int index)
+{
+	struct node* current;
+
+	current = (struct node*)malloc(sizeof(struct node*));
+	current->data.num = index;
+	current->next = NULL;
+	if (prev != NULL) prev->next = current;
+
+	return current;
 }
