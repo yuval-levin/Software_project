a9f08836d439a0c7c7f251600ecc335de97e4ade
yuval-levin <57755465+yuval-levin@users.noreply.github.com>
2020-08-30 18:37:37 +0300

    Calculation in ModularityMax
    Added first part of modularity calc
    Added malloc checkers

--- ModularityMaximization.c
+++ ModularityMaximization.c
@@ -18,6 +18,51 @@ void modularityMaximization(int* vectorS, struct divisionGroup* g)
 		}
 
 	}while(modularityChange > epsilon);
+
+	free(unmoved);
+}
+
+double* modularityTimesS(struct graph* graph,int* vectorS, struct divisionGroup* g)
+{
+	int i;
+	double* resVec = (double*)malloc(g->groupSize*sizeof(double));
+	if(resVec == NULL) exit(1); //TODO: print error before exit.
+	double* KiDividedByMPlusSum = secondArgumentInCalc(graph,vectorS,g);
+
+	for(i = 0 ; i < g->groupSize;i++)
+	{
+		resVec[i] = g->sumOfRows[i]*vectorS[i] + (graph->vectorDegrees) + KiDividedByMPlusSum[i];
+	}
+
+	free(KiDividedByMPlusSum);
+
+	return resVec;
+}
+
+//TODO: add explanation.
+double* secondArgumentInCalc(struct graph* graph,int* vectorS,struct divisionGroup* g)
+{
+	int i,index;
+	double  sum = 0;
+	double* KiDividedByMPlusSum = (double*)malloc(g->groupSize * sizeof(double));
+	if(KiDividedByMPlusSum == NULL) exit(1); //TODO: print error before exit.
+	struct spmat_node* current = g->groupSubmatrix->private[0];
+
+	for(i = 0;i < g->groupSize;i++)
+	{
+		//vectorDegrees is size Of number of nodes in the original A matrix
+		sum = sum + (vectorS[i]*graph->vectorDegrees[current->index]); //vectorS is size of g, we use i
+		current = current->next;
+		KiDividedByMPlusSum[i] = graph->vectorDegrees[current->index] / graph->M;
+	}
+	//two iterations are a must, cause we need to find sum first..
+	for(i = 0;i < g->groupSize;i++)
+	{
+		KiDividedByMPlusSum[i] = (graph->vectorDegrees[current->index] / graph->M)*sum;
+	}
+	return KiDividedByMPlusSum;
+
+
 }
 
 struct node* createUnmovedList(int sizeOfg)
@@ -37,6 +82,7 @@ struct node* appendToList(struct node* prev, int index)
 	struct node* current;
 
 	current = (struct node*)malloc(sizeof(struct node*));
+	if(current == NULL) exit(1); //TODO: print error before exit.
 	current->data.num = index;
 	current->next = NULL;
 	if (prev != NULL) prev->next = current;
