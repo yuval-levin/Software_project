e0b54a602286c2e950ed06134716c39644e5d4a2
YuvalMalka <57755334+YuvalMalka@users.noreply.github.com>
2020-09-18 16:13:31 +0300

    mod max optimization

--- spmat.h
+++ spmat.h
@@ -38,5 +38,6 @@ void add_row_of_size_n(struct _spmat *A, const double *row, int i, int n);
 struct spmat_node** get_private(struct _spmat* mat);
 void set_private(struct _spmat* mat, struct spmat_node** rows);
 void free_ll(struct _spmat *A);
+double sumTimesVectorHelper(struct spmat_node* cur_node, const double *v);
 
 #endif
