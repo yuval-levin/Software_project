2ceca4384514f67d2bc12156ffb1794f90a13c44
yuval-levin <57755465+yuval-levin@users.noreply.github.com>
2020-09-20 09:51:53 +0300

    Update xmacro_error_codes.h

--- xmacro_error_codes.h
+++ xmacro_error_codes.h
@@ -2,11 +2,11 @@
 #error "xmacro_error_codes.h included without defining ERROR_CODE"
 #endif
 
-ERROR_CODE(ERROR_INPUT_NOT_FOUND, "Input File not found")
-ERROR_CODE(ERROR_MALLOC_FAILED, "Memory allocation failed")
-ERROR_CODE(ERROR_DIVISION_BY_ZERO, "Invalid division by zero occured")
-ERROR_CODE(ERROR_READ_FAILED, "Reading from file failed")
-ERROR_CODE(ERROR_WRITE_FAILED, "Writing to file failed")
-ERROR_CODE(ERROR_OPEN_FAILED, "File opening failed")
-ERROR_CODE(ERROR_NUM_ARGS, "Cluster expected 3 Arguments, but received a different number")
-ERROR_CODE(ERROR_LOOP_LIMIT_REACHED, "Power iteration has entered infinite loop")
+ERROR_CODE(ERROR_INPUT_NOT_FOUND, "ERROR: Input File not found")
+ERROR_CODE(ERROR_MALLOC_FAILED, "ERROR: Memory allocation failed")
+ERROR_CODE(ERROR_DIVISION_BY_ZERO, "ERROR: Invalid division by zero occured")
+ERROR_CODE(ERROR_READ_FAILED, "ERROR: Reading from file failed")
+ERROR_CODE(ERROR_WRITE_FAILED, "ERROR: Writing to file failed")
+ERROR_CODE(ERROR_OPEN_FAILED, "ERROR: File opening failed")
+ERROR_CODE(ERROR_NUM_ARGS, "ERROR: Cluster expected 3 Arguments, but received a different number")
+ERROR_CODE(ERROR_LOOP_LIMIT_REACHED, "ERROR: Power iteration has entered infinite loop")
